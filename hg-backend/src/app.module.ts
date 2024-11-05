ba362dfe3a4631cab73c1db91cd95e11023b2856
joaomenna1 <joaomenna1@gmail.com>
2024-11-05 01:24:25 -0400

    update app module

--- hg-backend/src/app.module.ts
+++ hg-backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { UserModule } from './infra/htpp/modules/user/user.module';
+import { AuthModule } from './infra/htpp/modules/auth/auth.module';
+import { JwtAuthGuard } from './infra/htpp/modules/auth/guards/jwtAuth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
-  providers: [],
+  imports: [DatabaseModule, UserModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [],
 })
 export class AppModule {}
